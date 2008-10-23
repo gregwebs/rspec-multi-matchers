b0b19ae41ad0b5a6289068e3b5f97dc0a13e0784
Greg Weber <greg@grubtop.(none)>
2008-10-23 18:30:51 -0500

    executable README example for should each remove redundant tests

--- spec/match_each_spec.rb
+++ spec/match_each_spec.rb
@@ -1,14 +1,5 @@
 require 'spec/shared_enum_spec'
 
-describe MatchEnum do
-  it_should_behave_like "each matcher"
-
-  before do
-    @class = MatchEnum
-    @iterator = [:enum, :each]
-  end
-end
-
 describe MatchEach do
   it_should_behave_like "each matcher"
 
@@ -21,10 +12,11 @@ describe MatchEach do
     begin
       2.should == 1
     rescue Spec::Expectations::ExpectationNotMetError => e
+      @line = __LINE__ + 2
       lambda{ [1,2,3].should each { |n|
         n.should == 1
       } }.should raise_error(
-        Spec::Expectations::ExpectationNotMetError, /^\s*line: 25\s*item 1: 2\s*#{Regexp.escape(e.message)}/m)
+        Spec::Expectations::ExpectationNotMetError, /^\s*line: #{@line}\s*item 1: 2\s*#{Regexp.escape(e.message)}/m)
     else fail
     end
   end
